784b975f51c719f17b43f5b1aa1f17807654abcf
= <=>
2026-03-23 18:23:29 +0100

    Aplicación formulario con validación

--- src/app/components/barra-cerca/barra-cerca.component.ts
+++ src/app/components/barra-cerca/barra-cerca.component.ts
@@ -1,21 +1,29 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-barra-cerca',
   templateUrl: './barra-cerca.component.html',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   styleUrls: ['./barra-cerca.component.scss']
 })
-export class BarraCercaComponent {
 
+
+export class BarraCercaComponent {
   @Output() cerca = new EventEmitter<string>();
 
-  onCerca(event: any) {
-    this.cerca.emit(event.target.value);
+  texto: string = '';
+  onSubmit(){
+    this.cerca.emit(this.texto);
   }
+
+  //DESUSO
+  // onCerca(event: any) {
+  //   this.cerca.emit(event.target.value);
+  // }
 }
